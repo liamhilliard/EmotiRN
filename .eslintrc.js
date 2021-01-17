3219941c59df039ebb2a8c0d825d312cc290f45d
Liam Hilliard <liam.h.-@hotmail.com>
2021-01-17 16:59:18 +0000

    Update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    'root': true,
+    'extends': '@react-native-community',
+    'rules': {
+        'semi': [2, 'always'],
+        'indent': ['error', 4],
+        'comma-dangle': ['error', 'never']
+    }
 };

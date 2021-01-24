78138a3f75ef555bda8259e30ae6438dec9a8c66
Liam Hilliard <liam.h.-@hotmail.com>
2021-01-24 02:08:16 +0000

    Refactor SelectorGroup to create its own children

--- src/home/EmotionSelectorScreen.js
+++ src/home/EmotionSelectorScreen.js
@@ -1,56 +1,51 @@
 import React, {useState} from 'react';
 import {StyleSheet, ScrollView, Text, View, Button} from 'react-native';
-import EmotionSelectorGroup from './emotion-selector/EmotionSelectorGroup';
-import SelectorButton from './emotion-selector/SelectorButton';
-import {emotions, groups, colors} from '../core/emotions';
+import SelectorGroup from './emotion-selector/SelectorGroup';
+import {emotions, groups} from '../core/emotions';
+
+/*
+    TODO: Pass object to SelectorGroup instead of array of names
+    TODO: Fix button/AddEmotionalState screen
+ */
 
 export default function EmotionSelectorScreen({navigation, route}) {
-    const [selectedEmotions, setSelectedEmotions] = useState({});
+    const [selected, setSelected] = useState({});
 
     return (
         <ScrollView style={styles.container}>
             <View style={styles.titleContainer}>
                 <Text style={styles.titleText}>How do you feel?</Text>
             </View>
-            {Object.keys(groups).map((group) => {
+            <Text>{JSON.stringify(selected)}</Text>
+            {Object.keys(groups).map((groupName) => {
+                const currentGroup = groups[groupName];
                 return (
-                    <EmotionSelectorGroup
-                        selectedColor={colors[groups[group]]}
-                        key={group}
-                        title={group.toLowerCase()}
-                        children={Object.keys(emotions)
-                            .filter(
-                                (emotion) => emotions[emotion] === groups[group]
-                            )
-                            .map((name) => {
-                                return (
-                                    <SelectorButton
-                                        key={name}
-                                        name={name}
-                                        selectedColor={colors[groups[group]]}
-                                        selected={selectedEmotions[name]}
-                                        onPress={() => {
-                                            setSelectedEmotions({
-                                                ...selectedEmotions,
-                                                [name]: !selectedEmotions[name]
-                                            });
-                                        }}
-                                    />
-                                );
-                            })}
+                    <SelectorGroup
+                        color={currentGroup.color}
+                        key={groupName}
+                        title={groupName.toLowerCase()}
+                        selectorOptions={emotions
+                            .filter((emotion) => emotion.group === currentGroup)
+                            .map((emotion) => emotion.name)}
+                        onUpdate={(changes) => {
+                            setSelected({
+                                ...selected,
+                                ...changes
+                            });
+                        }}
                     />
                 );
             })}
-            <Button
+            {/* <Button
                 title="Continue"
                 onPress={() =>
                     navigation.navigate(
                         'Add Emotional State',
                         // eslint-disable-next-line prettier/prettier
-                        Object.keys(selectedEmotions)
+                        Object.keys(selected)
                     )
                 }
-            />
+            /> */}
         </ScrollView>
     );
 }
---
+++ src/home/emotion-selector/SelectorGroup.js
@@ -0,0 +1,118 @@
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faChevronUp, faChevronDown} from '@fortawesome/free-solid-svg-icons';
+import React, {useState, useEffect, useRef} from 'react';
+import {StyleSheet, Text, View, Pressable, Animated, Easing} from 'react-native';
+import SelectorButton from './SelectorButton';
+
+/*
+    TODO: Remove duplication between selected/not selected emotions
+    TODO: Refactor to only accept an object for selectorOptions
+*/
+
+export default function SelectorGroup({selectorOptions, onUpdate, title, color}) {
+    const [isOpen, setIsOpen] = useState(false);
+    const animHeight = useRef(new Animated.Value(0)).current;
+    const [options] = useState([...selectorOptions]);
+    const [values, setValues] = useState({});
+    const toggleOpen = () => {
+        setIsOpen(!isOpen);
+    };
+
+    useEffect(() => {
+        Animated.timing(animHeight, {
+            toValue: isOpen ? 1 : 0,
+            duration: 200,
+            easing: Easing.linear,
+            useNativeDriver: false
+        }).start();
+    }, [animHeight, isOpen]);
+
+    const maxHeight = animHeight.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 200]
+    });
+
+    return (
+        <View style={styles.container}>
+            <Pressable style={styles.titleContainer} onPress={toggleOpen}>
+                <Text style={[styles.titleText, {color}]}>{title}</Text>
+                <View style={styles.titleIcon}>
+                    <FontAwesomeIcon
+                        color={color}
+                        size={25}
+                        icon={isOpen ? faChevronUp : faChevronDown}
+                    />
+                </View>
+            </Pressable>
+            <View>
+                {options
+                    .filter((opt) => values[opt])
+                    .map((opt) => {
+                        return (
+                            <SelectorButton
+                                key={opt}
+                                name={opt}
+                                selectedColor={color}
+                                selected={values[opt]}
+                                onPress={() => {
+                                    const change = {[opt]: !values[opt]};
+                                    setValues({
+                                        ...values,
+                                        ...change
+                                    });
+                                    onUpdate(change);
+                                }}
+                            />
+                        );
+                    })}
+            </View>
+            <Animated.View style={[styles.childContainer, {maxHeight}]}>
+                {options
+                    .filter((opt) => !values[opt])
+                    .map((opt) => {
+                        return (
+                            <SelectorButton
+                                key={opt}
+                                name={opt}
+                                selectedColor={color}
+                                selected={values[opt]}
+                                onPress={() => {
+                                    const change = {[opt]: !values[opt]};
+                                    setValues({
+                                        ...values,
+                                        ...change
+                                    });
+                                    onUpdate(change);
+                                }}
+                            />
+                        );
+                    })}
+            </Animated.View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {},
+    titleContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    titleText: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        fontFamily: 'sans-serif-light',
+        marginBottom: 4,
+        marginStart: 4,
+        color: 'white'
+    },
+    titleIcon: {
+        justifyContent: 'center'
+    },
+    childContainer: {
+        overflow: 'hidden',
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    }
+});

7f759cf797648dec6cfb01f106a79a49f707d31c
Liam Hilliard <liam.h.-@hotmail.com>
2021-01-31 23:13:32 +0000

    Update button ripple color

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -6,11 +6,11 @@ const colors = {
     backgroundAltRipple: '#bfab8e',
 
     secondary: '#734e44',
-    secondaryAlt: '#bfab8e',
+    secondaryAlt: '#42302a',
 
     smiley: {
         background: '#f0d78b',
-        backgroundAlt: '#f0d28b',
+        backgroundAlt: '#ffe79e',
         foreground: '#383636',
         moods: {
             angry: '#f0958b',
